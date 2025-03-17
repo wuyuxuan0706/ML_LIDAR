87909e67abcd62ea3c747c41b72cca891a91bb61
achingto100 <wuyuxuanxxx0706@gmail.com>
2025-03-17 20:15:49 +0800

    3/17 save

---
+++ read_lidarData.py
@@ -0,0 +1,10 @@
+import numpy 
+import os 
+import struct as st
+
+def extract_para(file_path):
+    file_path = os.listdir(file_path)
+    print(file_path)
+    
+if "__name__" == "__main__":
+    extract_para("C:/Users/HP/Desktop/Python/Python-Projects/Python-Projects/ReadMcsFiles.py")
