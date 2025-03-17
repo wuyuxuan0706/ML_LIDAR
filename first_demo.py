4957353649c1939dfb937085cfa96fc1f10b897e
achingto100 <wuyuxuanxxx0706@gmail.com>
2025-03-17 19:01:42 +0800

    second save

--- first_demo.py
+++ first_demo.py
@@ -1,6 +1,10 @@
 def num_constant_output () :
     for i in range(10) :
+        for j in range(20) :
+            if i == 5 and j == 10 :
+                print("i = 5 and j = 10")
         print(i)
+    
     return 0
 
 if __name__ == "__main__" :
